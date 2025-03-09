1a82b58b788dc297d7a91d53f6755a292247ce08
IfraCreativeCoder <akhterifrah760@gmail.com>
2025-03-09 22:49:55 +0500

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,32 @@
+
+#Data Types
+
+#string
+name = "ifra Akhter"
+
+#integer
+age = 17
+
+#float
+price = 12.5
+
+#Boolean
+is_student = True
+
+#List
+Books = ["Python", "Java", "C++"]
+
+#Tuple
+fruits = ("Mango, Kiwi, Chikoo")
+
+#Dictionary
+student = {"name": "Zumar","subject":"Law"}
+
+#Set (unorded collection with unique items.)
+unique_numbers = {1,2,3,4,5}
+
+#None
+variable = None
+
+
+
